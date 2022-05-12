f09bef0d85b15f622e4bca5c47549830b10837f9
fdorel <dorelfurdui@gmail.com>
2022-05-12 14:09:12 -0400

    revised code

--- arrayOfMultiples.js
+++ arrayOfMultiples.js
@@ -1,14 +1,10 @@
 //  Array of Multiples - Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
 
-function arrayOfMultiples (num, length) {
-
-const multiples = (num, length) => {
+const arrayOfMultiples = (num, length) => {
    const result = []
    for (let i = num; i <= num * length; i += num){
       result.push(i)
    }
    return result
 };
-console.log(multiples(5, 8))
-}
-arrayOfMultiples();
+console.log(arrayOfMultiples(5, 8))
