4c1d608d9a4eb283c82b722685f7dfc48e985306
fdorel <dorelfurdui@gmail.com>
2022-05-17 11:47:22 -0400

    another exercise

---
+++ arrayLastItem.js
@@ -0,0 +1,13 @@
+// Create a function that returns the last element found in a specified array.
+
+const total = ['elem1', 'elem2', 'elem3']
+
+function lastItem (arr) {
+  return arr.at(-1)
+}
+const t1 = lastItem (total)
+console.log(t1)
+
+total.push('elem4')
+const t2 = lastItem (total)
+console.log(t2)
