07b0d8009b3b9e411a1cd97d459244c37e48f4da
Dorel Furdui <dorelfurdui@gmail.com>
2022-05-18 13:43:32 -0400

    Update checkIfInputArray.js

--- checkIfInputArray.js
+++ checkIfInputArray.js
@@ -1,1 +1,9 @@
+// Write a JavaScript function to check whether an 'input' is an array or not.
 
+var CheckArr = function(input) {
+	if (toString.call(input) === "[object Array]")
+	  	return true
+		return false
+	  }
+  console.log(CheckArr('Yes'))
+  console.log(CheckArr([55, 70, 9, 88,  -5]))
