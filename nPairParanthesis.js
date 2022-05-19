fef1a9cf840814b267a1e7ffac01525d4c0f7ae8
Dorel Furdui <dorelfurdui@gmail.com>
2022-05-19 15:03:20 -0400

    Update nPairParanthesis.js

--- nPairParanthesis.js
+++ nPairParanthesis.js
@@ -1,13 +1,15 @@
-function b(str) {
+//Create a function that reads "num" which will be an integer greater than or equal to zero, and return the number of valid combinations that can be formed with num pairs of parentheses.
+
+function b(num) {
     let rightParentheses = '(';
     let leftParentheses = ')';
     let rightCount = 0;
     let leftCount = 0;
 
-    for (var i = 0; i <= str.length; i++) {
-       if (rightParentheses == str.charAt(i))
+    for (var i = 0; i <= num.length; i++) {
+       if (rightParentheses == num.charAt(i))
           rightCount++;
-       else if (leftParentheses == str.charAt(i))
+       else if (leftParentheses == num.charAt(i))
           leftCount++;
        else if (rightCount < leftCount)
           return 0;
