98b0743523fad24aaeb2957abe0878cb063ffd92
Dorel Furdui <dorelfurdui@gmail.com>
2022-05-25 13:36:42 -0400

    Update nPairParanthesis.js

--- nPairParanthesis.js
+++ nPairParanthesis.js
@@ -25,4 +25,11 @@ function nPair(n) {
     var result = [];
     for (var i = 0; i < n; ++i) {
 
+        var lefts = nPair(i);
+        var rights = nPair(n - i - 1);
+
+        for (var l = 0; l < lefts.length; ++l)
+            for (var r = 0; r < rights.length; ++r)
+                result.push("(" + lefts[l] + ")" + rights[r]);
+    }
 
